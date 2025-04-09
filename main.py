643eab481adbba530c107eca823e702326ff04ea
human <tomaev.aleks@yandex.ru>
2025-04-10 00:21:14 +0300

    Created the database structure and the athlete model

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from data import db_session
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'AKFJDPOQWEXOFIJC_SDJKQ'
+
+@app.route('/')
+def index():
+    return 'Главная страница'
 
 
 def main():
-    app = Flask(__name__)
-
-    @app.route('/')
-    def main():
-        return 'Главная страница'
-
+    db_session.global_init("db/database.db")
     app.run(port=8080, debug=True)
 
 
