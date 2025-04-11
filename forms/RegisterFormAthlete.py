86f3f5d08e9a0756271a8d5c8cb54e517fc72369
human <tomaev.aleks@yandex.ru>
2025-04-11 13:42:16 +0300

    repair league choice in add_athlete.html

--- forms/RegisterFormAthlete.py
+++ forms/RegisterFormAthlete.py
@@ -1,5 +1,7 @@
+
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField, FloatField, RadioField
+from wtforms import StringField, SubmitField, DateField, FloatField, SelectField
 from wtforms.validators import DataRequired
 
 class RegisterFormAthlete(FlaskForm):
@@ -8,5 +10,5 @@ class RegisterFormAthlete(FlaskForm):
     surname = StringField('Фамилия', validators=[DataRequired()])
     birth_date = DateField('Дата рождения', validators=[DataRequired()])
     weight = FloatField('Вес', validators=[DataRequired()])
-    league = RadioField('Лига', validators=[DataRequired()]) # TODO Подключить radio button
+    league = SelectField('Лига', choices=[(0, 'А'), (1, 'Б'), (2, 'C'), (3, 'О')], validators=[DataRequired()]) # TODO Подключить radio button
     submit = SubmitField('Добавить спортсмена')
