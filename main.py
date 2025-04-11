2c69b7147d23041808b76e7752b942b03711d0c1
kIl4eR <khodiev.tamerlan@gmail.com>
2025-04-11 16:07:32 +0300

    add reg_user

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from data import db_session
 from forms.RegisterFormAthlete import RegisterFormAthlete
+from forms.RegisterFormUser import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'AKFJDPOQWEXOFIJC_SDJKQ'
@@ -14,6 +15,14 @@ def add_athlete():
     form = RegisterFormAthlete()
     if request.method == 'GET':
         return render_template('add_athlete.html', form=form)
+    else:
+        return 'success'
+
+@app.route('/reg_user', methods=['GET', 'POST'])
+def reg_athlete():
+    form = RegisterForm()
+    if request.method == 'GET':
+        return render_template('register_athlete.html', form=form)
     else:
         return 'success'
 
