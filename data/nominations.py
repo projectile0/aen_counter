00043988a121848c2b383780fd589e42d558cc15
human <tomaev.aleks@yandex.ru>
2025-04-13 01:59:55 +0300

    nominations.py: add base Nomination class

---
+++ data/nominations.py
@@ -0,0 +1,11 @@
+import sqlalchemy as sa
+
+from .db_session import SqlAlchemyBase
+
+class Nomination(SqlAlchemyBase):
+    __tablename__ = 'nominations'
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    type = sa.Column(sa.Integer, nullable=False)
+    athletes = sa.Column(sa.String, nullable=False)
+    finished = sa.Column(sa.Integer)
