013674882204b4ebd236edc4e6537fe266266cbb
victo <victor.sergeeff@gmail.cpom>
2025-04-12 15:49:28 +0300

    Edited Navbar and moved it into base

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = 'AKFJDPOQWEXOFIJC_SDJKQ'
 
 @app.route('/')
 def index():
-    return render_template('navbar.html')
+    return render_template('base.html')
 
 
 def main():
