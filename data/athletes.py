d2be98dab2070d6226693043d029172c8ee70dcc
human <tomaev.aleks@yandex.ru>
2025-04-12 14:36:53 +0300

    move RegisterFormAthlete into athletes.py and RegisterFormUser into users.py

--- data/athletes.py
+++ data/athletes.py
@@ -1,5 +1,7 @@
 import sqlalchemy as sa
+
 from .db_session import SqlAlchemyBase
+
 
 class Athlete(SqlAlchemyBase):
     __tablename__ = 'athletes'
@@ -11,4 +13,16 @@ class Athlete(SqlAlchemyBase):
     weight = sa.Column(sa.Float)
     league = sa.Column(sa.Integer)
 
-def add_athlete(name):
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateField, FloatField, SelectField
+from wtforms.validators import DataRequired, InputRequired
+
+
+class RegisterFormAthlete(FlaskForm):
+    name = StringField('Имя', validators=[InputRequired()])
+    surname = StringField('Фамилия', validators=[InputRequired()])
+    birth_date = DateField('Дата рождения', validators=[DataRequired()])
+    weight = FloatField('Вес', validators=[InputRequired()])
+    league = SelectField('Лига', choices=[(0, 'А'), (1, 'Б'), (2, 'C'), (3, 'О')], validators=[DataRequired()])
+    submit = SubmitField('Добавить спортсмена')
--- data/users.py
+++ data/users.py
@@ -1,6 +1,8 @@
 import sqlalchemy as sa
-from .db_session import SqlAlchemyBase
 from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db_session import SqlAlchemyBase
+
 
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
@@ -8,9 +10,20 @@ class User(SqlAlchemyBase):
     username = sa.Column(sa.String, nullable=False, unique=True)
     hashed_password = sa.Column(sa.String, nullable=False)
 
-
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
+
+
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class RegisterFormUser(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
